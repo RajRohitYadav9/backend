b7142fd3ed872bd16b02c566d7e027a1a16aa662
RajRohitYadav9 <rjrohityadav19@gmail.com>
2024-07-05 12:06:49 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 from datetime import timedelta
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True, resources={r"/*": {"origins": "https://agreeable-ground-084082a1e.5.azurestaticapps.net/"}})
+CORS(app, supports_credentials=True)
 app.config['SECRET_KEY'] = 'my_secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SESSION_TYPE'] = 'filesystem'
